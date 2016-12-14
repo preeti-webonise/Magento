9daaec2165bd2d89337fbf539713d708fa7f94a4
preeti-webonise <preeti.bhosale@weboniselab.com>
2016-12-14 19:33:55 +0530

    Custom_Module : Create Custom Module in magento

---
+++ Preeti/HelloWorld/Helper/Data.php
@@ -0,0 +1,5 @@
+<?php
+class Preeti_HelloWorld_Helper_Data extends Mage_Core_Helper_Abstract {
+
+}
+?>
---
+++ Preeti/HelloWorld/controllers/Adminhtml/IndexController.php
@@ -0,0 +1,10 @@
+<?php
+class Preeti_HelloWorld_Adminhtml_IndexController extends Mage_Adminhtml_Controller_Action {
+
+  public function indexAction() {
+
+  echo 'hello world in the admin side..!!!'. '';
+
+  }
+}
+?>
